550cf5781442105c9d5d4cf81422497d08446ba0
Mr-Rana-Coder <mukulguptadb@gmail.com>
2024-12-04 13:22:18 +0530

    New Error Handling Files Added

--- src/index.js
+++ src/index.js
@@ -5,4 +5,12 @@ dotenv.config({
     path:"./env"
 })
 
-connectDB();  
+connectDB().
+then(()=>{
+    app.listen(process.env.PORT || 9000,()=>{
+        console.log("Server is Running",process.env.PORT);
+    })
+}).
+catch((err)=>{
+    console.log("Mongo Connection Error : ",err);
+})
